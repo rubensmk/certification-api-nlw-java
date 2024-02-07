eb80b693bc0137ae4bc81dc3a345792ea9fd8925
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-02-07 01:00:02 -0300

    ✅ Finalizado Aula 1

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,32 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertification;
+
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
+
+        var result = this.verifyIfHasCertification.execute(verifyHasCertificationDTO);
+
+        if (result) {
+            return "Usuário já fez a prova!";
+        }
+
+        return "Usuário pode fazer a prova";
+    }
+
+}
