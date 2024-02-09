0163e4af2133ec255fbeea046b8ebf5faa8d4009
Rubens Mititaka Kishimoto <rubenskishimoto@gmail.com>
2024-02-08 23:52:46 -0300

    ✅ Aula 2 - Criando as tabelas das entities

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,15 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +17,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String email;
-    private List<CertificationStudentEntity> certificationStudentEntity;
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "studentEntity")
+    private List<CertificationStudentEntity> certificationStudentEntity;
 }
